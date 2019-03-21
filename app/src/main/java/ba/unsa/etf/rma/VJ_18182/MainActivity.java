8ee82eefbd3d1b55d4268fb4b97585df6cb111d6
Dzavid <dbrdar1@etf.unsa.ba>
2019-03-21 23:30:41 +0100

    Constraint liste promijenjen

--- app/src/main/java/ba/unsa/etf/rma/VJ_18182/MainActivity.java
+++ app/src/main/java/ba/unsa/etf/rma/VJ_18182/MainActivity.java
@@ -1,7 +1,7 @@
 package ba.unsa.etf.rma.VJ_18182;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,28 +12,28 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private EditText editText;
-    private ListView listView;
+    private Button dugme;
+    private EditText tekst;
+    private ListView lista;
     private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
-        editText = (EditText) findViewById(R.id.editText);
-        listView = (ListView) findViewById(R.id.listView);
+        dugme = (Button) findViewById(R.id.button);
+        tekst = (EditText) findViewById(R.id.editText);
+        lista = (ListView) findViewById(R.id.listView);
         final ArrayList<String> unosi = new ArrayList<>();
         adapter = new ArrayAdapter<>
                 (this,  android.R.layout.simple_list_item_1, unosi);
-        listView.setAdapter(adapter);
-        button.setOnClickListener(new View.OnClickListener() {
+        lista.setAdapter(adapter);
+        dugme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                unosi.add(0, editText.getText().toString());
+                unosi.add(0, tekst.getText().toString());
                 adapter.notifyDataSetChanged();
-                editText.setText("");
+                tekst.setText("");
             }
         });
     }
